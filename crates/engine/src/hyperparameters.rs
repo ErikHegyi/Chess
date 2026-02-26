3aee372d398d90f44a4b7b0d55569f046c84c83e
ErikHegyi <hegyi.erik.2k8@gmail.com>
2026-02-26 15:28:43 +0100

    Added more hyperparameters

--- crates/engine/src/hyperparameters.rs
+++ crates/engine/src/hyperparameters.rs
@@ -1,3 +1,6 @@
 pub const KERNEL_SIZE: i64 = 3;
-pub const ALPHA: f64 = 0.9;
+pub const ALPHA: f32 = 0.2;
 pub const LAMBDA: f64 = 1e-3;
+pub const MU: f32 = 0.01;
+pub const OMICRON: f32 = 0.0005;
+pub const KAPPA: f32 = 0.1;
